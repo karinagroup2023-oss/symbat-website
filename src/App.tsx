5b259829f96a7f1c382c93be61274e555e6d5c15
karinagroup2023-oss <karina.group2023@gmail.com>
2026-06-14 16:24:24 +0500

    feat: compose full landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,31 @@
-export default function App() { return <h1 className="font-display">Сымбат</h1> }
+import { Header } from './sections/Header'
+import { Hero } from './sections/Hero'
+import { Pains } from './sections/Pains'
+import { WhyNotOrdinary } from './sections/WhyNotOrdinary'
+import { Services } from './sections/Services'
+import { Passport } from './sections/Passport'
+import { Process } from './sections/Process'
+import { Audience } from './sections/Audience'
+import { About } from './sections/About'
+import { FinalCta } from './sections/FinalCta'
+import { Footer } from './sections/Footer'
+
+export default function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Hero />
+        <Pains />
+        <WhyNotOrdinary />
+        <Services />
+        <Passport />
+        <Process />
+        <Audience />
+        <About />
+        <FinalCta />
+      </main>
+      <Footer />
+    </>
+  )
+}
